5ce84862645eb50f88976c72bdae920614904d9c
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-03-03 22:01:08 -0500

    add thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,45 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
-const Thought = model('thought', new Schema({}));
+const Thought = model('thought', new Schema(
+		{
+			thoughtText: {
+				type: String,
+				required: true,
+				min: 1,
+				max: 280,
+			},
+			createdAt: {
+				type: Date,
+				default: Date.now,
+				get: date =>
+					date.toLocaleString('en-US', {
+						dateStyle: 'medium',
+						timeStyle: 'short'
+					}),
+			},
+			username: {
+				type: String,
+				required: true,
+			},
+			reactions: {
+				type: [reactionSchema],
+			},
+		},
+		{
+			virtuals: {
+				reactionCount: {
+					get() {
+						return this.reactions?.length || 0;
+					},
+				},
+			},
+			toJSON: {
+				getters: true,
+			},
+			id: false,
+		}
+	)
+);
 
 module.exports = Thought;
