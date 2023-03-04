373e150daf96818f4404d45e65f819594a3d7117
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-03-03 22:00:43 -0500

    add user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,50 @@
 const { Schema, model } = require('mongoose');
 
-const User = model('user', new Schema({}));
+const isEmail = /^((?:[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){2,})$/;
+
+const User = model('user', new Schema(
+		{
+			username: {
+				type: String,
+				unique: true,
+				required: true,
+				trim: true,
+			},
+			email: {
+				type: String,
+				unique: true,
+				required: true,
+				match: isEmail,
+			},
+
+			thoughts: [
+				{
+					type: Schema.Types.ObjectId,
+					ref: 'thought',
+				},
+			],
+
+			friends: [
+				{
+					type: Schema.Types.ObjectId,
+					ref: 'user',
+				},
+			],
+		},
+		{
+			virtuals: {
+				friendCount: {
+					get() {
+						return this.friends?.length || 0;
+					},
+				},
+			},
+			toJSON: {
+				getters: true,
+			},
+			id: false,
+		}
+	)
+);
 
 module.exports = User;
