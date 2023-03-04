62c589d4d77216deb5c66af5d41c11f2b4f3e740
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2023-03-03 23:22:32 -0500

    add user routes and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,58 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
-module.exports = {};
+module.exports = {
+	// Get all users
+	getUsers(req, res) {
+		User.find()
+			.then(users => res.json(users))
+			.catch(err => res.status(500).json(err));
+	},
+
+	// Get a single user
+	getSingleUser(req, res) {
+		User.findOne({ _id: req.params.userId })
+			.then(user =>
+				!user
+					? res.status(404).json({ message: 'No user with that ID!' })
+					: res.json(user)
+			)
+			.catch(err => res.status(500).json(err));
+	},
+
+	// Create a new user
+	createUser(req, res) {
+		User.create(req.body)
+			.then(user => res.json(user))
+			.catch(err => res.status(500).json(err));
+	},
+
+    updateUser(req,res) {
+        User.findByIdAndUpdate(
+			req.params.userId,
+			{ $set: req.body },
+			{ runValidators: true, new: true }
+		)
+			.then(user =>
+				!user
+					? res.status(404).json({ message: 'No user with that ID!' })
+					: res.json(user)
+			)
+			.catch(err => res.status(500).json(err));
+    },
+
+	// Delete a user and associated thoughts
+	deleteUser(req, res) {
+		User.findOneAndDelete({ _id: req.params.userId })
+			.then(user =>
+				!user
+					? res.status(404).json({ message: 'No user with that ID!' })
+					: Thought.deleteMany({
+							_id: { $in: user.thoughts },
+					  })
+			)
+			.then(() =>
+				res.json({ message: 'User and associated thoughts deleted!' })
+			)
+			.catch(err => res.status(500).json(err));
+	},
+};
